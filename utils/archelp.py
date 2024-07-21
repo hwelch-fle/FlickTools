eab8e8560acc5ead39edf6b465cc0064e4abe76a
Hayden Welch <hwelch@finelines-engineering.com>
2024-07-21 15:41:23 -0400

    rename module

--- utils/archelp.py
+++ utils/archelp.py
@@ -1,13 +1,31 @@
 import arcpy
 import os
+import builtins
 
+from typing import Any, Generator, Literal
+from enum import Enum
 import utils.constants.ftconstants as ftconstants
 
 # Use this module for any helper functions or classes that you want to use 
 # between tools. If you find yourself re-impelementing the same function in 
 # multiple tools, consider moving it here.
 
-def row_to_dict(cursor: arcpy.da.SearchCursor) -> dict:
+class controlCLSID(Enum):
+    """ See [Parameter Controls](https://pro.arcgis.com/en/pro-app/latest/arcpy/geoprocessing_and_python/parameter-controls.htm)
+        documentation for more information on parameter controls.
+    """
+    EXCLUDE_INTERSECT_AND_UNION = '{15F0D1C1-F783-49BC-8D16-619B8E92F668}'
+    SLIDER_RANGE = '{C8C46E43-3D27-4485-9B38-A49F3AC588D9}'
+    LARGE_NUMBER = '{7A47E79C-9734-4167-9698-BFB00F43AE41}'
+    COMPOSITE_SWITCH = '{BEDF969C-20D2-4C41-96DA-32408CA72BF6}'
+    MULTILINE = '{E5456E51-0C41-4797-9EE4-5269820C6F0E}'
+    MULTIVALUE_CHECKBOX = '{172840BF-D385-4F83-80E8-2AC3B79EB0E0}'
+    MULTIVALUE_CHECK_ALL = '{38C34610-C7F7-11D5-A693-0008C711C8C1}'
+    FEATURE_LAYER_CREATE = '{60061247-BCA8-473E-A7AF-A2026DDE1C2D}'
+    HORIZONTAL_VALUE_TABLE = '{1AA9A769-D3F3-4EB0-85CB-CC07C79313C8}'
+    SINGLE_VALUE_TABLE = '{1A1CA7EC-A47A-4187-A15C-6EDBA4FE0CF7}'
+
+def row_to_dict(cursor: arcpy.da.SearchCursor) -> Generator[dict[str, Any], None, None]:
     """
     Converts a arcpy.da Cursor row to a dictionary
 
@@ -111,6 +129,32 @@ def msg(message: str="", level: str="message") -> None:
         arcpy.AddError(message)
     return
 
+# Override alternative print function that works as a full print and arcpy.AddMessage
+def print(*values: object,
+          sep: str = " ",
+          end: str = "\n",
+          file = None,
+          flush: bool = False,
+          severity: Literal['INFO', 'WARNING', 'ERROR'] = None):
+    """ Print a message to the ArcGIS Pro message queue and stdout
+    set severity to 'WARNING' or 'ERROR' to print to the ArcGIS Pro message queue with the appropriate severity
+    """
+        
+    # Print the message to stdout
+    builtins.print(*values, sep=sep, end=end, file=file, flush=flush)
+    
+    end = "" if end is '\n' else end
+    message = f"{sep.join(map(str, values))}{end}"
+    # Print the message to the ArcGIS Pro message queue with the appropriate severity
+    match severity:
+        case "WARNING":
+            arcpy.AddWarning(f"{message}")
+        case "ERROR":
+            arcpy.AddError(f"{message}")
+        case _:
+            arcpy.AddMessage(f"{message}")
+    return
+
     
 def get_params(parameters: list, filter_list: list[str]=None) -> dict:
     """
@@ -128,7 +172,7 @@ def get_params(parameters: list, filter_list: list[str]=None) -> dict:
 
     return params
 
-def get_rows(features: str, fields: list[str], query: str=None)-> dict:
+def get_rows(features: str, fields: list[str], query: str=None)-> Generator[dict[str, Any], None, None]:
     """
     Gets the rows from the feature class
 
@@ -147,7 +191,7 @@ def get_rows(features: str, fields: list[str], query: str=None)-> dict:
         for row in row_to_dict(cursor):
             yield cursor, row
                 
-def update_rows(features: str, fields: list[str], query: str=None) -> dict:
+def update_rows(features: str, fields: list[str], query: str=None) -> Generator[dict[str, Any], None, None]:
     """
     Updates the rows in the feature class
 
@@ -254,33 +298,3 @@ def print_dict(dict_to_print: dict, tab_num: int = 0, tab: str = ftconstants.TAB
     dict_string = [f"{tab_padding}{k:<{key_padding}}: {v}\n" for k, v in dict_to_print.items()]
     
     return "".join(dict_string)
-
-# def print_layout(layout=None, page_name:str="LAYOUT", quality:str="BEST", resolution:int=300, is_mapseries:bool=False):
-#     """
-#     Prints the layout to a PDF
-#     @layout: The layout to print
-#     @quality: The quality of the PDF (BEST, NORMAL, FASTEST)
-#     @resolution: The resolution of the PDF (DPI)
-#     @is_mapseries: Is the layout part of a map series (default is False)
-#     @return: The PDF document
-#     """
-#     temp = tempfile.mkdtemp()
-#     if is_mapseries:
-#         mapseries = layout
-#         pdf = os.path.join(temp, f"{page_name}.pdf")
-#         return mapseries.exportToPDF(pdf, resolution=resolution, image_quality=quality, page_range_type="CURRENT")
-#     else:
-#         pdf = os.path.join(temp, f"{layout.name}.pdf")
-#         return layout.exportToPDF(pdf, resolution=resolution, image_quality=quality)
-        
-# def print_mapseries(mapseries=None, quality:str="BEST", resolution:int=300):
-#     """
-#     Iterates over a map series and prints to a list of PDFs
-#     @mapseries: The map series to print
-#     @quality: The quality of the PDF (BEST, NORMAL, FASTEST)
-#     @resolution: The resolution of the PDF (DPI)
-#     @return: A list of PDF documents
-#     """
-#     for page_num in range(1, mapseries.pageCount+1):
-#         mapseries.currentPageNumber = page_num
-#         yield print_layout(mapseries, quality, resolution, is_mapseries=True)
